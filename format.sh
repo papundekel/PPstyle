d5e76bfb782fe11f5d3aa7d931ccadeedf0eb8cf
Peter Fačko <pfacko1@gmail.com>
2021-11-08 16:53:54 +0100

    .

--- format.sh
+++ format.sh
@@ -4,4 +4,6 @@ cd $( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
 
 cp ./format ../.clang-format && \
 \
-find ../ -regex ".*\.[ch]\(pp\)?" | xargs clang-format -i
+find ../ -regex ".*\.[ch]\(pp\)?" | xargs clang-format -i && \
+\
+rm ../.clang-format
