d29bcbdf7770cb154803343165fffab2cdc1502a
Peter Fačko <pfacko1@gmail.com>
2021-11-11 14:06:04 +0100

    .

--- PPstyle.cmake
+++ PPstyle.cmake
@@ -1,12 +1,17 @@
 cmake_minimum_required(VERSION 3.20)
 
+set(_install_prefix "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../")
+
 function(PPstyle TARGET_NAME)
-    get_target_property(DIR "${TARGET_NAME}" SOURCE_DIR)
+    get_target_property(_sources "${TARGET_NAME}" SOURCES)
+    get_target_property(_interface_sources "${TARGET_NAME}" INTERFACE_SOURCES)
+    list(APPEND _all_sources _sources ${_interface_sources})
+    list(REMOVE_DUPLICATES _all_sources)
 
     add_custom_target("${TARGET_NAME}-style"
-        COMMAND "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../bin/PPstyle.sh" "${DIR}"
+        COMMAND "${_install_prefix}/bin/PPstyle.sh" ${_all_sources}
         COMMENT "Formatting ${TARGET_NAME}"
-        WORKING_DIRECTORY "${CMAKE_CURRENT_FUNCTION_LIST_DIR}"
+        WORKING_DIRECTORY "${_install_prefix}"
     )
     add_dependencies("${TARGET_NAME}" "${TARGET_NAME}-style")
 endfunction()
--- PPstyle.sh
+++ PPstyle.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if (($# != 1)) || [ ! -d $1 ]; then
-    echo "Usage: format.sh DIR" >&2
-    exit 2
-fi
+echo "$@"
 
-ln -s "$(pwd)/.clang-format" "$1/" && \
-\
-find "$1" -regex ".*\.[ch]\(pp\)?" | xargs clang-format -i && \
-\
-rm "$1/.clang-format"
+# if (($# != 1)) || [ ! -d $1 ]; then
+#     echo "Usage: format.sh DIR" >&2
+#     exit 2
+# fi
+
+# ln -s "$(pwd)/.clang-format" "$1/" && \
+# \
+# find "$1" -regex ".*\.[ch]\(pp\)?" | xargs clang-format -i && \
+# \
+# rm "$1/.clang-format"
